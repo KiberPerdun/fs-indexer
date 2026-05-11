7dbeedb3353cf407e23229881260757cbb44d903
KiberPerdun <suisuikin@yandex.ru>
2026-05-11 12:34:24 +0300

    Small MediaServer HTTP GET /media_files mutex lock ctx fix

--- src/server.cpp
+++ src/server.cpp
@@ -11,8 +11,11 @@ MediaServer::run (const std::string &host, const int32_t port)
            [this] (const httplib::Request &, httplib::Response &res)
              {
                std::string data_to_send;
-               std::lock_guard<std::mutex> lock (mtx);
-               res.set_content (data, "application/json");
+               {
+                 std::lock_guard<std::mutex> lock (mtx);
+                 data_to_send = data;
+               }
+               res.set_content (data_to_send, "application/json");
              });
 
   svr.listen (host, port);
